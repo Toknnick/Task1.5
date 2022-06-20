bdc6cd1c1d7da707b4d2edd33861c5c3b6b19725
Maksim <maksimus.tumannick@yandex.ru>
2022-06-20 19:36:06 +0300

    Task5

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Юшин";
+            string secondName = "Максим";
+            Console.WriteLine("До :");
+            Console.WriteLine(name);
+            Console.WriteLine(secondName);
+            Console.WriteLine();
+
+            string swapping;
+            swapping = name;
+            name = secondName;
+            secondName = swapping;
+            Console.WriteLine("После :");
+            Console.WriteLine(name);
+            Console.WriteLine(secondName);
+        }
+    }
+}
